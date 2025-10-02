d2f8846a542780021ac7a3350edce7ec128b2cea
Simon Leary <simon.leary42@proton.me>
2025-10-02 21:40:30 +0000

    add plugin file

---
+++ coldfront/config/plugins/allocation_openldap.py
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: (C) ColdFront Authors
+#
+# SPDX-License-Identifier: AGPL-3.0-or-later
+
+from coldfront.config.base import INSTALLED_APPS
+from coldfront.config.env import ENV
+
+INSTALLED_APPS += ["coldfront.plugins.project_openldap"]
+
+# where allocation gid numbering will start, no default value provided here on purpose, site should define sensible value
+ALLOCATION_OPENLDAP_GID_START = ENV.int("ALLOCATION_OPENLDAP_GID_START")
+# control the length of allocation title component from CF which is inserted into the OpenLDAP description field for each OpenLDAP allocation and is potentially truncated down
+ALLOCATION_OPENLDAP_DESCRIPTION_TITLE_LENGTH = ENV.int("ALLOCATION_OPENLDAP_DESCRIPTION_TITLE_LENGTH", default=100)
