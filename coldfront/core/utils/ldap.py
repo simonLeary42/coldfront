e3cd5f28b5186ffcc8020dc48d6d97f6711ac1da
Simon Leary <simon.leary42@proton.me>
2025-10-08 00:07:18 +0000

    more ldap

--- coldfront/core/utils/ldap.py
+++ coldfront/core/utils/ldap.py
@@ -1,82 +1,79 @@
+import logging
 from functools import wraps
 
 from ldap3 import MODIFY_ADD, MODIFY_DELETE, MODIFY_REPLACE, Connection
-from ldap3.core.exceptions import LDAPException
-from ldap3.protocol.rfc4511 import LDAPResult
 
-# from ldap3 import MODIFY_ADD, MODIFY_DELETE, MODIFY_REPLACE, Connection, Server, Tls
-
-# openldap_connection
-# add_members_to_openldap_posixgroup
-# remove_members_from_openldap_posixgroup
-# add_per_project_ou_to_openldap
-# add_posixgroup_to_openldap
-# remove_dn_from_openldap
-# update_posixgroup_description_in_openldap
-# move_dn_in_openldap
-# ldapsearch_check_project_dn
-# ldapsearch_check_ou
-# ldapsearch_get_posixgroup_memberuids
-# ldapsearch_get_description
-# allocate_project_openldap_gid
-# construct_ou_dn_str
-# construct_ou_archived_dn_str
-# construct_dn_str
-# construct_dn_archived_str
-# construct_per_project_ou_relative_dn_str
-# construct_project_ou_description
-# construct_project_posixgroup_description
+logger = logging.getLogger(__name__)
 
 
-def _ensure_ldap_success(func):
+def _noop_if_values_nonlist_or_empty(func):
     @wraps(func)
-    def wrapper(*args, **kwargs):
-        result = func(*args, **kwargs)
-        if result.get("result") != 0:
-            raise LDAPException(result)
-        return result
+    def wrapper(conn: Connection, dn: str, attribute_name: str, values, controls=None) -> None:
+        if isinstance(values, list) and (len(values) == 0):
+            func(conn, dn, attribute_name, values, controls=None)
+        logger.error(
+            "values is a non-list or an empty list, nothing doing...",
+            stack_info=True,
+            extra={"dn": dn, "attribute_name": attribute_name, "values": values},
+        )
 
     return wrapper
 
 
-def _ensure_nonempty_list(x, name):
-    if not isinstance(x, list):
-        raise TypeError(f"{name} must be a list!")
-    if len(x) == 0:
-        raise ValueError(f"{name} cannot be empty!")
+def _noop_if_conn_none(func):
+    @wraps(func)
+    def wrapper(conn: Connection, *args, **kwargs) -> None:
+        if conn is not None:
+            func(conn * args, **kwargs)
+        logger.error("conn is None, nothing doing...", stack_info=True, extra=dict(args=args, kwargs=kwargs))
+
+    return wrapper
 
 
-@_ensure_ldap_success
-def add_attribute_values(conn: Connection, dn: str, attribute_name: str, values, controls=None) -> LDAPResult:
-    """multi-valued attributes only"""
-    _ensure_nonempty_list(values, "values")
+def _log_ldap_errors(func):
+    @wraps(func)
+    def wrapper(conn: Connection, *args, **kwargs) -> None:
+        func(conn, *args, **kwargs)
+        if conn.result["result"] != 0:
+            logger.error(f"LDAP operation failed! {conn.result}", stack_info=True)
+
+    return wrapper
+
+
+@_noop_if_values_nonlist_or_empty
+@_noop_if_conn_none
+@_log_ldap_errors
+def add_attribute_values(conn: Connection, dn: str, attribute_name: str, values, controls=None) -> None:
     conn.modify(dn, {attribute_name: [MODIFY_ADD, values]}, controls=controls)
-    return conn.result
 
 
-@_ensure_ldap_success
-def overwrite_attribute_values(conn: Connection, dn: str, attribute_name: str, values, controls=None) -> LDAPResult:
-    """multi-valued attributes only"""
-    _ensure_nonempty_list(values, "values")
+@_noop_if_values_nonlist_or_empty
+@_noop_if_conn_none
+@_log_ldap_errors
+def overwrite_attribute_values(conn: Connection, dn: str, attribute_name: str, values, controls=None) -> None:
     conn.modify(dn, {attribute_name: [MODIFY_REPLACE, values]}, controls=controls)
-    return conn.result
 
 
-@_ensure_ldap_success
-def delete_attribute_values(conn: Connection, dn: str, attribute_name: str, values: list, controls=None) -> LDAPResult:
-    """multi-valued attributes only"""
-    _ensure_nonempty_list(values, "values")
+@_noop_if_values_nonlist_or_empty
+@_noop_if_conn_none
+@_log_ldap_errors
+def delete_attribute_values(conn: Connection, dn: str, attribute_name: str, values: list, controls=None) -> None:
     conn.modify(dn, {attribute_name: [MODIFY_DELETE, values]}, controls=controls)
-    return conn.result
 
 
-@_ensure_ldap_success
-def move_entry(conn: Connection, dn: str, rdn: str, destination_ou_dn: str) -> LDAPResult:
+@_noop_if_conn_none
+@_log_ldap_errors
+def create_entry(conn: Connection, dn: str, object_class: list, attributes: dict, controls=None) -> None:
+    conn.add(dn, object_class=object_class, attributes=attributes, controls=controls)
+
+
+@_noop_if_conn_none
+@_log_ldap_errors
+def move_entry(conn: Connection, dn: str, rdn: str, destination_ou_dn: str) -> None:
     conn.modify_dn(dn, rdn, new_superior=destination_ou_dn)
-    return conn.result
 
 
-@_ensure_ldap_success
-def delete_entry(conn: Connection, dn: str, controls=None) -> LDAPResult:
+@_noop_if_conn_none
+@_log_ldap_errors
+def delete_entry(conn: Connection, dn: str, controls=None) -> None:
     conn.delete(dn, controls=controls)
-    return conn.result
