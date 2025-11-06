a5caea6a7aa508858f2fba211b6966136376da75
Simon Leary <simon.leary42@proton.me>
2025-11-06 10:14:53 -0500

    remove bad django_get_or_create
    Signed-off-by: Simon Leary <simon.leary42@proton.me>

--- coldfront/core/test_helpers/factories.py
+++ coldfront/core/test_helpers/factories.py
@@ -141,7 +141,6 @@ class ProjectStatusChoiceFactory(DjangoModelFactory):
 class ProjectFactory(DjangoModelFactory):
     class Meta:
         model = Project
-        django_get_or_create = ("title",)
 
     pi = SubFactory(UserFactory)
     title = factory.Faker("project_title")
@@ -254,7 +253,6 @@ class AllocationStatusChoiceFactory(DjangoModelFactory):
 class AllocationFactory(DjangoModelFactory):
     class Meta:
         model = Allocation
-        django_get_or_create = ("project",)
 
     justification = factory.Faker("sentence")
     status = SubFactory(AllocationStatusChoiceFactory)
